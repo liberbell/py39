ead95940d37ccd503f61401dec53be38345ead67
Liberbell <liberbell@gmail.com>
2022-05-28 13:47:41 +0900

    news0202

--- step1/news02.py
+++ step1/news02.py
@@ -2,3 +2,10 @@ import newspaper
 
 URL = "https://www.bloomberg.co.jp/"
 website = newspaper.build(URL)
+
+for article in website.articles:
+    article.download()
+    article.parse()
+    article.nlp()
+    print(article.title)
+    print(article.url)
