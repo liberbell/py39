fcfb193dcb8a7782d27f4b5e2d1af8218ab1dbb2
Liberbell <liberbell@gmail.com>
2022-05-28 13:52:03 +0900

    news0205

--- step1/news02.py
+++ step1/news02.py
@@ -3,10 +3,16 @@ import newspaper
 URL = "https://www.bloomberg.co.jp/"
 website = newspaper.build(URL, memoize_articles = False)
 
+i = 0
+
 for article in website.articles:
     article.download()
     article.parse()
     article.nlp()
-    print(article.title)
+    print("Article" ,str(i), ":", article.title)
     print(article.url)
     print(article.summary, end="\n\n")
+
+    if i > 9:
+        break
+    i = i + 1
