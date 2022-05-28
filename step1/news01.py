50008633bc2f7fa5f62aee076d1dad6009b36852
Liberbell <liberbell@gmail.com>
2022-05-28 13:28:47 +0900

    news012

--- step1/news01.py
+++ step1/news01.py
@@ -2,3 +2,5 @@ from newspaper import Article
 
 URL = "https://www.reuters.com/world/europe/ukraine-says-troops-may-retreat-eastern-region-russia-advances-2022-05-28/"
 article = Article(URL)
+article.download()
+article.parse()
