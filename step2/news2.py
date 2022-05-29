1fa3a5745b761891effd988211bcc82c98ff1e6a
Liberbell <liberbell@gmail.com>
2022-05-30 08:35:20 +0900

    news203

--- step2/news2.py
+++ step2/news2.py
@@ -1,8 +1,15 @@
-from operator import imod
 from newspaper import Article
 import newspaper
+import nltk
 
 URL1 = "https://www.itmedia.co.jp/"
 URL2 = "https://thebridge.jp/"
 
 website = newspaper.build(URL1, memoize_articles = False)
+
+i = 0
+for article in website.articles:
+    article.download()
+    article.parse()
+    article.nlp()
+    print("Article", ":", article.title)
