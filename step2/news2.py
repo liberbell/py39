0b327248c82d1d9b9b8a3a0a8238d27635fdcd51
Liberbell <liberbell@gmail.com>
2022-05-30 08:43:27 +0900

    news206

--- step2/news2.py
+++ step2/news2.py
@@ -4,18 +4,21 @@ import nltk
 
 URL1 = "https://www.itmedia.co.jp/"
 URL2 = "https://thebridge.jp/"
+URLs = ["https://www.itmedia.co.jp/", "https://thebridge.jp/"]
 
-website = newspaper.build(URL1, memoize_articles = False)
+# website = newspaper.build(URL1, memoize_articles = False)
 
-i = 0
-for article in website.articles:
-    article.download()
-    article.parse()
-    article.nlp()
-    print("Article", ":", article.title)
-    print(article.url)
-    print(article.summary, end="\n")
+for url in URLs:
+    websites = newspaper.build(url, memoize_articles = False, language="ja")
+    i = 0
+    for article in websites.articles:
+        article.download()
+        article.parse()
+        article.nlp()
+        print("Article", ":", article.title)
+        print(article.url)
+        print(article.summary, end="\n")
 
-    if i > 9:
-        break
-    i = i + 1
+        if i > 9:
+            break
+        i = i + 1
