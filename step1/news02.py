7ebaec84e357b40d5fdb3eb18d55b3fd478fe3ab
Liberbell <liberbell@gmail.com>
2022-05-29 17:25:35 +0900

    news0204

--- step1/news02.py
+++ step1/news02.py
@@ -6,6 +6,14 @@ import datetime
 URL = "https://www.bloomberg.co.jp/"
 website = newspaper.build(URL, memoize_articles = False)
 
+csv_date = datetime.datetime.today().strftime("%Y%m%d")
+csv_file_name = "Bloomberg_" + csv_date
+
+f = open(csv_file_name, "w", encoding="cp932", errors="ignore")
+writer = csv.writer(f, lineterminator="\n")
+csv_header = ["Aricle No", "Title", "URL", "Summary"]
+writer.writerow(csv_header)
+
 i = 0
 
 for article in website.articles:
@@ -19,10 +27,3 @@ for article in website.articles:
     if i > 9:
         break
     i = i + 1
-
-csv_date = datetime.datetime.today().strftime("%Y%m%d")
-csv_file_name = "Bloomberg_" + csv_date
-
-f = open(csv_file_name, "w", encoding="cp932", errors="ignore")
-writer = csv.writer(f, lineterminator="\n")
-csv_header = ["Aricle No", "Title", "URL", "Summary"]
