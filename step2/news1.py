566bc6311c13951e0431040a66c7e69857c91012
Liberbell <liberbell@gmail.com>
2022-05-29 17:39:30 +0900

    news104

--- step2/news1.py
+++ step2/news1.py
@@ -1,8 +1,9 @@
 from newspaper import Article
 import nltk
 
-URL = "https://www.itmedia.co.jp/"
-article = Article(URL)
+URL = "https://www.itmedia.co.jp/mobile/articles/2205/27/news028.html"
+
+article = Article(URL, memoize_articles = False)
 article.download()
 article.parse()
 print(article.authors)
