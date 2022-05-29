a5cba7defc97f6b0e476f891406bd0ade61c4072
Liberbell <liberbell@gmail.com>
2022-05-29 17:27:45 +0900

    news0206

--- step1/news02.py
+++ step1/news02.py
@@ -28,7 +28,10 @@ for article in website.articles:
     csvlist.append(article.title)
     csvlist.append(article.url)
     csvlist.append(article.summary)
+    writer.writerow(csvlist)
 
     if i > 9:
         break
     i = i + 1
+
+f.close()
