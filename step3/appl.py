39d023f38ae3df5e34625ad9cb8f857231afe39d
Liberbell <liberbell@gmail.com>
2022-05-30 08:58:29 +0900

    apple2

--- step3/appl.py
+++ step3/appl.py
@@ -1,3 +1,6 @@
 import pandas as pd
+import requests
 
 URL = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
+
+data = pd.read_html(requests.get(URL, headers={'User-agent': 'Mozilla/5.0'}).text,header=0)
