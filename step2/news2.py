b503c5a8f47d7fd93260259f03870227f2d391be
Liberbell <liberbell@gmail.com>
2022-05-30 08:32:59 +0900

    news202

--- step2/news2.py
+++ step2/news2.py
@@ -1,5 +1,8 @@
+from operator import imod
 from newspaper import Article
+import newspaper
 
 URL1 = "https://www.itmedia.co.jp/"
 URL2 = "https://thebridge.jp/"
 
+website = newspaper.build(URL1, memoize_articles = False)
