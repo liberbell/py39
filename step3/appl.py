c6dd793c444ec8fb095053e66d3ff59b8401d5a2
Liberbell <liberbell@gmail.com>
2022-05-31 08:46:34 +0900

    appl6

--- step3/appl.py
+++ step3/appl.py
@@ -6,17 +6,20 @@ URL = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
 
 data = pd.read_html(requests.get(URL, headers={'User-agent': 'Mozilla/5.0'}).text,header=0)
 
-print(data[0].head())
-print(data[0].tail())
+# print(data[0].head())
+# print(data[0].tail())
 
 data[0]["Adj Close**"] = pd.to_numeric(data[0]["Adj Close**"], errors="coerce")
-print(data[0].tail())
+# print(data[0].tail())
 
 data[0].dropna(inplace=True)
-print(data[0].tail())
+# print(data[0].tail())
 
 data[0]["Date2"] = [dt.strptime(i, "%b %d, %Y") for i in data[0]["Date"]]
-print(data[0].tail())
+# print(data[0].tail())
 
 data[0].set_index("Date2", inplace=True)
-print(data[0].tail())
+# print(data[0].head())
+# print(data[0]["Adj Close**"].dtype)
+
+data[0]["Adj Close**"].plot(title="APPL Stock Price", grid=True)
