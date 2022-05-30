b7fabfab6bd0b328d54de79c827d892294c0e1a0
Liberbell <liberbell@gmail.com>
2022-05-31 08:31:58 +0900

    appl1

--- step3/appl.py
+++ step3/appl.py
@@ -7,3 +7,6 @@ data = pd.read_html(requests.get(URL, headers={'User-agent': 'Mozilla/5.0'}).tex
 
 print(data[0].head())
 print(data[0].tail())
+
+data[0]["Adj Close**"] = pd.to_numeric(data[0]["Adj Close**"], errors="coerce")
+print(data[0].tail())
