cf2cd66e4eef388d09b33147133e69bafa330d55
Liberbell <liberbell@gmail.com>
2022-05-31 08:59:54 +0900

    datapick3

--- step4/datapick.py
+++ step4/datapick.py
@@ -1,7 +1,12 @@
+from pydoc import resolve
 import requests
 
 URL = "https://www.yahoo.co.jp"
 
 response = requests.get(URL)
 print(response.status_code)
-print(response.text)
+# print(response.text)
+
+# print(response.content)
+print(response.encoding)
+print(response.headers)
