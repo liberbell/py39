c34e1944e06aea596f236c5306f75b71cb013a6c
Liberbell <liberbell@gmail.com>
2022-06-03 08:54:20 +0900

    newsget4

--- step5/news_get.py
+++ step5/news_get.py
@@ -6,4 +6,13 @@ URL = "https://www.yomiuri.co.jp/"
 
 response1 = requests.get(URL)
 print(response1.status_code)
-print(response1.get)
+# print(response1.text)
+
+soup = BeautifulSoup(response1.text, "html.parser")
+
+# body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(1) > div > h3 > a
+# body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(2) > div > h3 > a
+# body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(3) > div > h3 > a
+
+elems = soup.select("div.headline > article:nth-child(1) > div > h3 > a")
+print(elems[0])
