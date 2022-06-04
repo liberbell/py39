c2096baf36f772753188c93af2fa0ff2d6d81637
Liberbell <liberbell@gmail.com>
2022-06-04 10:08:43 +0900

    yahooget6

--- step6/yahooget.py
+++ step6/yahooget.py
@@ -1,3 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
+# #tabpanelTopics1 > div > div._2jjSS8r_I9Zd6O9NFJtDN- > ul > li:nth-child(1) > article > a > div > div > h1 > span
+URL = "https://www.yahoo.co.jp/"
+
+yahoo_response = requests.get(URL)
+soup = BeautifulSoup(yahoo_response, "html.parser")
+
+elms = soup.find_all("a")
+print(elms)
