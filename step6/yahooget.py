c3cafe1dd357fe65aaebc8b3f9dd71cab0a2596e
Liberbell <liberbell@gmail.com>
2022-06-04 15:24:25 +0900

    yahooget15

--- step6/yahooget.py
+++ step6/yahooget.py
@@ -34,4 +34,5 @@ for pickup_link in pickup_links:
     pickup_soup = BeautifulSoup(pickup_response.text, "html.parser")
     pickup_elem = pickup_soup.find("p", class_="sc-kvjbNB igDfka")
     # <p class="sc-kvjbNB igDfka"><a href="https://news.yahoo.co.jp/articles/0a62dabf598b8790042dcfa5b96c1d42cff5bb40" data-ylk="rsec:tpc_main;slk:headline;pos:2;" data-ual-gotocontent="true" class="sc-kREsUy fOkZWA" data-rapid_p="40">記事全文を読む</a></p>
-    
+    news_link = pickup_elem.a.attrs["href"]
+    print(news_link)
