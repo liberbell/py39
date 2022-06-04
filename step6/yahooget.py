bbcdbece6dbb1cedff44b6965a07df9614848ec9
Liberbell <liberbell@gmail.com>
2022-06-04 10:13:41 +0900

    yahooget8

--- step6/yahooget.py
+++ step6/yahooget.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import re
 
 # #tabpanelTopics1 > div > div._2jjSS8r_I9Zd6O9NFJtDN- > ul > li:nth-child(1) > article > a > div > div > h1 > span
 URL = "https://www.yahoo.co.jp/"
@@ -9,4 +10,7 @@ yahoo_response = requests.get(URL)
 soup = BeautifulSoup(yahoo_response.text, "html.parser")
 
 elms = soup.find_all("a")
-print(elms)
+# print(elms)
+
+elems = soup.find_all(href=re.compile("yahoo.co.jp/pickup"))
+print(elems[0].span.string)
