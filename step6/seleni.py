212b4915c65a24e97404e6e236e29016ee72e8e4
Liberbell <liberbell@gmail.com>
2022-06-05 09:09:45 +0900

    seleni2

--- step6/seleni.py
+++ step6/seleni.py
@@ -4,5 +4,7 @@ from time import sleep
 
 driver = webdriver.Safari()
 URL = "https://www.yahoo.co.jp"
+URL2 = "https://www.google.com"
 
-driver.get(URL)
+driver.get(URL2)
+search_bar = driver.find_element_by_name("q")
