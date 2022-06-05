64adbf98604d4667083f41b31b6d72634db644cf
Liberbell <liberbell@gmail.com>
2022-06-05 12:41:04 +0900

    seleni9

--- step6/seleni.py
+++ step6/seleni.py
@@ -1,15 +1,15 @@
-from lib2to3.pgen2 import driver
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
 
-driver = webdriver.Safari()
+wdriver = webdriver.Safari()
 URL = "https://www.yahoo.co.jp"
 URL2 = "https://www.google.com"
 
-driver.get(URL2)
-search_bar = driver.find_element(By.NAME, value = "q")
+wdriver.get(URL2)
+search_bar = wdriver.find_element(By.NAME, "q")
 search_bar.send_keys("Python")
 search_bar.submit()
 
-for elem_h3 in driver.find_elements_by_xpath("//a/h3"):
+for elem_h3 in wdriver.find_elements_by_xpath("//a/h3"):
     print(elem_h3.text)
