b95acf5fc8e1d2c660b5009340f57d6da6df928b
Liberbell <liberbell@gmail.com>
2022-06-05 09:15:41 +0900

    seleni5

--- step6/seleni.py
+++ step6/seleni.py
@@ -7,6 +7,9 @@ URL = "https://www.yahoo.co.jp"
 URL2 = "https://www.google.com"
 
 driver.get(URL2)
-search_bar = driver.find_element_by_name("q")
+search_bar = driver.find_element("q")
 search_bar.send_keys("Python")
 search_bar.submit()
+
+for elem_h3 in driver.find_elements("//a/h3"):
+    print(elem_h3.text)
