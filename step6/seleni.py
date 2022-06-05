af455bdaaa6f44ff789dc0381fef078d1c3c0bb4
Liberbell <liberbell@gmail.com>
2022-06-06 08:49:58 +0900

    seleni7

--- step6/seleni.py
+++ step6/seleni.py
@@ -11,11 +11,13 @@ search_bar = wdriver.find_element(By.NAME, "q")
 search_bar.send_keys("Python")
 search_bar.submit()
 
-# elem = wdriver.find_elements(By.XPATH, "//a/h3")
+elem = wdriver.find_elements(By.XPATH, "//a/h3")
+print(type(elem))
+print(elem)
 # elem = wdriver.find_element_by_xpath("//a/h3")
 
 #rso > div:nth-child(1) > div > div > div > div > div > div.yuRUbf > a > h3
 #rso > div:nth-child(1) > div > div > div > div > div > div.yuRUbf > a > h3
 # print(wdriver.find_element(By.XPATH, "//a/h3"))
-for elem_h3 in wdriver.find_elements(By.XPATH, "//a/h3"):
-    print(elem_h3.text)
+# for elem_h3 in wdriver.find_elements(By.XPATH, "//a/h3"):
+#     print(elem_h3.text)
