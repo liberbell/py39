998c1b0f36eebea6788005dc27f46a10c6babf3f
Liberbell <liberbell@gmail.com>
2022-06-07 08:42:02 +0900

    try14

--- step7/try1.py
+++ step7/try1.py
@@ -6,6 +6,6 @@ URL = "https://kanagawa-park.jp/bbq/yoyaku/hatano/calender.html&kind=1"
 bbqdata = requests.get(URL)
 print(bbqdata.status_code)
 
-bbqdata_soup = BeautifulSoup(bbqdata, "html.parser")
+bbqdata_soup = BeautifulSoup(bbqdata.text, "html.parser")
 elem = bbqdata_soup.find_all("td")
 print(elem)
