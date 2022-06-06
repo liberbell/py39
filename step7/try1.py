17a7bdb7ec33daa9cbcc547f9bcec2e29b4bc168
Liberbell <liberbell@gmail.com>
2022-06-07 08:59:48 +0900

    try18

--- step7/try1.py
+++ step7/try1.py
@@ -7,10 +7,12 @@ bbqdata = requests.get(URL)
 print(bbqdata.status_code)
 
 bbqdata_soup = BeautifulSoup(bbqdata.text, "html.parser")
+print(bbqdata_soup.prettify())
 elem1 = bbqdata_soup.find_all("td")
 elem2 = bbqdata_soup.select("tbody > tr:nth-of-type(1) > td:nth-of-type(6) > a")
 print(elem2)
 
+#main > section > div.sn > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(6) > a
 #main > section > div.sn > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(6) > a
    #main > ul > li:nth-child(1) > a
 # div#main > ul.items > li
