e0346d46e5940a5fb7528189eec1baaeeadac963
Liberbell <liberbell@gmail.com>
2022-06-08 08:33:46 +0900

    try11

--- step7/try1.py
+++ step7/try1.py
@@ -12,11 +12,12 @@ elem1 = bbqdata_soup.find_all("td")
 elem2 = bbqdata_soup.select("tr:nth-child(2)> td > a")
 elem3 = bbqdata_soup.find_all("a")
 print(elem2)
+print(elem2[0])
 # print(elem3)
-for elem in elem2:
+# for elem in elem2:
     # print(elem.a.string)
     # print(elem.a["href"])
-    print(elem[0].a)
+    # print(elem[0].a)
 
 
 #main > section > div.sn > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(6) > a
