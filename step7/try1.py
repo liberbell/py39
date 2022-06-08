e254834ebce1f110a6cf1d621f1e24b10e5eec41
Liberbell <liberbell@gmail.com>
2022-06-08 09:06:32 +0900

    try18

--- step7/try1.py
+++ step7/try1.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 URL = "https://kanagawa-park.jp/bbq/yoyaku/hatano/calender.html&kind=1"
 
@@ -11,8 +12,8 @@ bbqdata_soup = BeautifulSoup(bbqdata.text, "html.parser")
 elem1 = bbqdata_soup.find_all("td")
 elem2 = bbqdata_soup.select("td > a")
 elem3 = bbqdata_soup.find_all("a")
-elem4 = bbqdata_soup.select('img[alt="1部"]')
-print(elem4)
+# elem4 = bbqdata_soup.select('img[alt="1部"]')
+# print(elem4)
 # print(elem2)
 # print(elem2[0])
 # print(elem3)
@@ -27,6 +28,12 @@ print(elem4)
    #main > ul > li:nth-child(1) > a
 # div#main > ul.items > li
 i = 0
-for i in range(len(elem4)):
-    print(elem4[i].attrs["href"])
-    i = i + 1
+search_section = "1部"
+search_days = (19)
+# for i in range(len(elem2)):
+
+#     print(elem2[i])
+#     i = i + 1
+
+section_list = [list1 for list1 in elem2 if search_section in list1]
+print(section_list)
