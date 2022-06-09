8cdfd83759d538bde735c8a8e67ac1d723065a09
Liberbell <liberbell@gmail.com>
2022-06-10 08:46:28 +0900

    try13

--- step7/try1.py
+++ step7/try1.py
@@ -34,10 +34,10 @@ search_day = 19
 # search_keyword = "d=" + search_day
 # print(search_keyword)
 for i in range(len(elem2)):
-    url_parse = urlparse(elem2[i])
+    url_parse = urlparse(str(elem2[i]))
     print(url_parse)
     # print(elem2[i])
-    print(grep_target)
+    # print(grep_target)
     i = i + 1
 
 # for i in range(len(elem4)):
