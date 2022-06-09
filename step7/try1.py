18e7936908a57887bb55cf135717f06a53eb34ae
Liberbell <liberbell@gmail.com>
2022-06-09 09:03:27 +0900

    try17

--- step7/try1.py
+++ step7/try1.py
@@ -34,8 +34,9 @@ search_keyword = "d=" + search_day
 print(search_keyword)
 for i in range(len(elem2)):
     if elem2[i] in search_keyword:
+        grep_target = elem2[i]
     # print(elem2[i])
-    print(GREP_TARGET)
+    print(grep_target)
     i = i + 1
 
 # for i in range(len(elem4)):
