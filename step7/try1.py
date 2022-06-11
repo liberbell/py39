ae59d8884e0bc9c63da068b61657c7d3611133db
Liberbell <liberbell@gmail.com>
2022-06-11 10:16:25 +0900

    try15

--- step7/try1.py
+++ step7/try1.py
@@ -55,7 +55,7 @@ while True:
         # print(elem2[i])
         # print(grep_target)
         i = i + 1
-        time.sleep(15)
+        time.sleep(30)
         print(match_count)
         if match_count != 0:
             beep(2000, 100)
